0f55d5c9eb752959890d4d2da6af89f1960116b0
Micheal <micheal@visionmasterdesigns.com>
2014-02-04 22:38:31 -0800

    added epub task in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp        = require('gulp');
+var gutil       = require('gulp-util');
 var clean       = require('gulp-clean');
 var concat      = require('gulp-concat');
 var pandoc      = require('gulp-pandoc');
@@ -9,6 +10,17 @@ var srcFromToc  = require('./gulp-plugin/gulp-parse-toc')
 
 const DEST_DIR = "./dist";
 
+/*
+ * List all tasks
+**/
+gulp.task('default', function () {
+  gutil.log("Available Tasks:");
+  Object.keys(gulp.tasks).forEach(function (taskName) {
+    if ( taskName.indexOf('generate') > -1 ) {
+      gutil.log("\t", gutil.colors.yellow(taskName));
+    }
+  });
+});
 
 /*
  * Removes the distribution (dist/) directory.
@@ -34,7 +46,7 @@ gulp.task('concat', ['clean'], function () {
  * [TODO]: This may change based on how the book website
  * may look.
 **/
-gulp.task('concat:html', ['concat'], function () {
+gulp.task('generate:html', ['concat'], function () {
   return gulp.src(['dist/index.md'])
     .pipe(markdown())
     .pipe(gulp.dest(DEST_DIR))
@@ -44,12 +56,26 @@ gulp.task('concat:html', ['concat'], function () {
  * Converts the concatenated markdown (index.md) to
  * index.pdf
 **/
-gulp.task('concat:pdf', ['concat'], function () {
+gulp.task('generate:pdf', ['concat'], function () {
   const CSS_PATH = __dirname + '/build-assets/pdf.css';
-  
+
   return gulp.src(['dist/index.md'])
     .pipe(markdownpdf({
       cssPath: CSS_PATH
     }))
     .pipe(gulp.dest(DEST_DIR))
+});
+
+/*
+ * Converts the concatenated markdown (index.md) to
+ * index.epub
+**/
+gulp.task('generate:epub', ['concat'], function () {
+  return gulp.src(['dist/index.md'])
+    .pipe(pandoc({
+      from: 'markdown',
+      to  : 'epub',
+      ext : '.epub',
+      args: ['-o', './dist/index.epub', '--latex-engine', 'xelatex']
+    }));
 });
